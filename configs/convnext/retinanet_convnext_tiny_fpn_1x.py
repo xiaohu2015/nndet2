1d0ca55a6c4d9874e3978de0a155fea0a7003e18
Hu Ye <xiaohuzc@gmail.com>
2022-01-14 10:01:31 +0800

    Create retinanet_convnext_tiny_fpn_1x.py

---
+++ configs/convnext/retinanet_convnext_tiny_fpn_1x.py
@@ -0,0 +1,43 @@
+from ..common.coco_schedule import lr_multiplier_1x as lr_multiplier
+from ..common.data.coco import dataloader
+from ..common.models.retinanet import model
+from ..common.train import train
+
+import torch
+from detectron2.config import LazyCall as L
+from detectron2.solver.build import get_default_optimizer_params
+from detectron2.modeling.backbone.fpn import LastLevelP6P7
+
+from nndet2.modeling.backbone import ConvNeXt
+
+
+model.backbone.bottom_up = L(ConvNeXt)(
+    in_chans=3,
+    depths=[3, 3, 9, 3],
+    dims=[96, 192, 384, 768], 
+    drop_path_rate=0.4,
+    layer_scale_init_value=1.0,
+    out_features=["stage2", "stage3", "stage4"],
+)
+model.backbone.top_block=L(LastLevelP6P7)(
+    in_channels=768,
+    out_channels="${..out_channels}",
+    in_feature="stage4"
+)
+model.pixel_mean = [123.675, 116.28, 103.53]
+model.pixel_std = [58.395, 57.12, 57.375]
+model.input_format = "RGB"
+
+optimizer = L(torch.optim.AdamW)(
+    params=L(get_default_optimizer_params)(
+        # params.model is meant to be set to the model object, before instantiating
+        # the optimizer.
+        weight_decay_norm=0.0
+    ),
+    lr=1e-04,
+    weight_decay=0.05,
+    betas=(0.9, 0.999),
+)
+
+dataloader.train.mapper.image_format = "RGB"
+dataloader.train.mapper.use_instance_mask = False
